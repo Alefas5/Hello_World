638deff51e3fec4e9fc07699605ac9c0cffd23b0
Alexandros Katsianas <119041902+Alefas5@users.noreply.github.com>
2025-03-15 13:58:19 +0200

    Add files via upload

---
+++ Alexandros.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alexandros : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello Alexandros");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("crickets");
+    }
+}
